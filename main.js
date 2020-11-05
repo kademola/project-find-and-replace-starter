0c67172049ad974e5be68515a26c91c7a1ed000e
Ademola Kolawole <kademola@Wales-MBP.home>
2020-11-05 10:48:21 -0500

    script

--- main.js
+++ main.js
@@ -19,22 +19,6 @@ function getCellElements(currentRowElement) {
   return currentRowElement.querySelectorAll(".cell");
 }
 
-// YOUR CODE GOES HERE
-
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
-
 replaceAllButton.addEventListener("click", function () {
   let findInputResult = findInput.value;
   let replaceInputResult = replaceInput.value;
